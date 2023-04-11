e828a34dc017f5a3b27544172b8b61ccdf339c8e
Anton Gridushko <anton.gridushko@gmail.com>
2023-04-11 22:03:41 +0300

    Refactor middleware and braod composition root due SOC.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
+// Import the required namespaces
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+
+// ...
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var services = builder.Services;
+services.AddControllersWithViews();
 
-builder.Services.AddControllersWithViews();
+// Access configuration values
+var config = builder.Configuration;
+var staticFilesDirectory = config.GetValue<string>("StaticFilesDirectory");
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
 
+// Use the configuration value to set the file provider
+app.UseStaticFiles(new StaticFileOptions
+{
+	FileProvider = new PhysicalFileProvider(
+		Path.Combine(app.Environment.ContentRootPath, staticFilesDirectory)
+	)
+});
+
+app.UseRouting();
 
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html"); ;
+app.MapFallbackToFile("index.html");
 
 app.Run();
