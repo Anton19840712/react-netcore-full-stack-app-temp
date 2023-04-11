af3bbdeda6a8819b7a201d7c51f9e45871c3f7e6
Anton Gridushko <anton.gridushko@gmail.com>
2023-04-11 22:39:07 +0300

    Revert "Refactor middleware and broad composition root due SOC."
    This reverts commit 7d44b402092b7f0a68a7673d5abc9c0244aea9ad.

--- Program.cs
+++ Program.cs
@@ -1,43 +1,27 @@
-// Import the required namespaces
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
-
-// ...
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var services = builder.Services;
-services.AddControllersWithViews();
 
-// Access configuration values
-var config = builder.Configuration;
-var staticFilesDirectory = config.GetValue<string>("StaticFilesDirectory");
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-// Use the configuration value to set the file provider
-app.UseStaticFiles(new StaticFileOptions
-{
-	FileProvider = new PhysicalFileProvider(
-		Path.Combine(app.Environment.ContentRootPath, staticFilesDirectory)
-	)
-});
-
+app.UseStaticFiles();
 app.UseRouting();
+
 
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");
+app.MapFallbackToFile("index.html"); ;
 
 app.Run();
